5785b7854f178a6d7ac99d5159671463d639b64a
Charlotte Kieffer <kindred242@gmail.Com>
2023-02-01 16:49:57 +0100

    created the contact component and the style

---
+++ frontend/src/components/ContactForm.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "../assets/styles/contactForm.css";
+
+export default function ContactForm() {
+  const [formData, setFormData] = useState({
+    nom: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="contact">
+      <h2>Me contacter</h2>
+      <form className="formulaire" onSubmit={handleSubmit}>
+        <label className="form-item">
+          Nom:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="form-item">
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="form-item">
+          Message:
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
+        <button className="submit" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
