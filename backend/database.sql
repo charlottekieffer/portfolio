80f023a297160c989b4c2873df6fe662c0bbd72c
Charlotte Kieffer <kindred242@gmail.Com>
2023-02-01 18:58:21 +0100

    created the Controllers and Manager for the skill table

--- backend/database.sql
+++ backend/database.sql
@@ -1,10 +1,3 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
-
 CREATE TABLE user
 (
 	id             INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -46,3 +39,21 @@ CREATE TABLE skill
   type
 ) VALUES (1, 'React', 'hard'), (2, 'HTML', 'hard'), (3, 'CSS', 'hard'), (4, 'Express', 'hard'), (5, 'Javascript', 'hard'), (6, "Sens de l'organisation", 'soft'), 
 (7, "Esprit d'équipe", 'soft'), (8, "Excellente communication", 'soft'), (9, "Aisance relationnelle", 'soft');
+
+CREATE TABLE project
+(
+	id        INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	title      VARCHAR(100) NOT NULL,
+	description      VARCHAR(900) NOT NULL
+) engine = InnoDB
+  default charset = latin1;
+
+  INSERT INTO project (
+  id,
+  title,
+  description
+  ) VALUES 
+  (1, "Book of the Wilders", "Création d'un annuaire pour présenter les personnes de la promo 2022/2023 de notre formation de développeur web et mobile."),
+  (2, "Find Fuel", "Création d'un site permettant de recenser les stations-service sous forme de liste ou de carte, avec les différents carburants disponibles ainsi que leur prix."),
+  (3, "Origins Digital", "Plateforme de streaming de vidéos sportives avec la possibilité de s'enregistrer pour avoir accès à toutes les vidéos de la plateforme.
+  Création d'un back-office pour que les administrateurs du site puissent gérer les vidéos et les utilisateurs.")
--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 const userControllers = require("./controllers/userControllers");
+const skillControllers = require("./controllers/skillControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
@@ -16,5 +17,11 @@ router.get("/users/:id", userControllers.read);
 router.put("/users/:id", userControllers.edit);
 router.post("/users", userControllers.add);
 router.delete("/users/:id", userControllers.destroy);
+
+router.get("/skills", skillControllers.browse);
+router.get("/skills/:id", skillControllers.read);
+router.put("/skills/:id", skillControllers.edit);
+router.post("/skills", skillControllers.add);
+router.delete("/skills/:id", skillControllers.destroy);
 
 module.exports = router;
