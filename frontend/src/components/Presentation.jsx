9f589582502660ccc83d5f9f4231df6eac410b3c
Charlotte Kieffer <kindred242@gmail.Com>
2023-02-01 14:42:19 +0100

    created the presentation component and the style

---
+++ frontend/src/components/Presentation.jsx
@@ -0,0 +1,17 @@
+import "../assets/styles/presentation.css";
+
+export default function VideosByCategory() {
+  return (
+    <div className="presentation">
+      <h1>Bonjour! Je m'appelle Charlotte</h1>
+      <p className="about">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <h2>Mes compétences</h2>
+      <p className="skills">CSS HTML REACT EXPRESS </p>
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Navbar from "../components/Navbar";
+import Presentation from "../components/Presentation";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <Navbar />
-    </header>
+    <>
+      <header className="App-header">
+        <Navbar />
+      </header>
+      <main>
+        <Presentation />
+      </main>
+    </>
   );
 }
